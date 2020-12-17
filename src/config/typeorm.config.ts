5b9455054f1dae92905802a9bff154b8ed7ae9f7
hiep294 <quanghiep294@gmail.com>
2020-12-17 22:39:36 +0700

    connect_to_pg

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'taskmanagement',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'], // **: means all folders, *.entity.ts means files has ext is entity.js or entity.ts
+  synchronize: true,
+};
